9053a34fe3107e7399972d16c336d74a985db72b
Stefanos <stefanos.kouroupis@cloudcall.com>
2023-10-04 08:17:14 +0000

    fix entry file

--- lua/telescope/_extensions/kubels.lua
+++ lua/telescope/_extensions/kubels.lua
@@ -1,11 +1,18 @@
-local has_telescope, _ = pcall(require, 'telescope')
+local has_telescope, telescope = pcall(require, 'telescope')
 
 if not has_telescope then
 	error('Please install nvim-telescope/telescope.nvim if you want to use this plugin.')
 end
 
+local opts = require("telescope.themes").get_dropdown {}
+
+local kubels = function()
+  local kls = require("kubels")
+  kls.setup(opts)
+end
+
 return telescope.register_extension {
 	exports = {
-		kube_nvim = require("kubels").setup(require("telescope.themes").get_dropdown {})
+		kubels =  kubels 
 	}
 }
